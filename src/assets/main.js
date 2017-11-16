535301a9a8e0cfeebd68b84deb2d522e0b1f456a
jangdmn <31955136+jangdmn@users.noreply.github.com>
2017-11-16 14:42:22 +0100

    change #1

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,5 +2,36 @@ $.ajax({
     url: 'https://www.codeschool.com/users/4147696.jsonp',
     dataType: 'jsonp',
     success: function(response) {
+    	addCourses (response.courses.completed);
     }
   });
+function addCourses(course) {
+
+	var $badges = $('#badges');
+
+	courses.forEach(function(course) {
+
+
+		var $course = $('<div />', 
+			'class': 'course'
+			}).appendTo($badges);
+
+		$('<h3 />', {
+			text: course.title
+		}).appendTo($course);
+
+		$('<img />', {
+			src: course.badge
+		}).appendTo($course);
+
+		$('<a />', {
+			'class': 'btn btn-primary',
+			target: '_blank',
+			href: course.url,
+			text: 'See Course'
+		}).appendTo($course);
+
+		
+		})
+	}
+});
